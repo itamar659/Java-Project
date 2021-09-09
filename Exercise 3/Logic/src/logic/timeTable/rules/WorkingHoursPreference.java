76307aac1ca71e2983e77d2c6acd290137db4015
itamar659 <itamar.ayalon@gmail.com>
2021-09-09 21:01:07 +0300

    Added rule
    -Implemented working hours preference rule

--- Exercise 3/Logic/src/logic/timeTable/rules/WorkingHoursPreference.java
+++ Exercise 3/Logic/src/logic/timeTable/rules/WorkingHoursPreference.java
@@ -1,9 +1,15 @@
 package logic.timeTable.rules;
 
+import logic.timeTable.Lesson;
+import logic.timeTable.Teacher;
 import logic.timeTable.TimeTable;
 import logic.timeTable.rules.base.Rule;
 import engine.base.Solution;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class WorkingHoursPreference extends Rule<TimeTable> {
 
@@ -14,6 +20,32 @@ public class WorkingHoursPreference extends Rule<TimeTable> {
 
     @Override
     public float calcFitness(Solution<TimeTable> solution) {
-        throw new NotImplementedException();
+        List<Lesson> lessons = new ArrayList<>(solution.getGens().getLessons());
+
+        int max = 0;
+        int penalty;
+
+        // build a map from teacher id to live working hours
+        Map<String, Integer> teacherID2workingHours = new HashMap<>();
+        for (Teacher teacher1 : solution.getGens().getProblem().getTeachers()) {
+            teacherID2workingHours.put(teacher1.getId(), 0);
+            max += teacher1.getWorkingHours();
+        }
+
+        lessons.forEach(lesson -> {
+            int workingHours = teacherID2workingHours.get(lesson.getTeacher().getId());
+            teacherID2workingHours.put(lesson.getTeacher().getId(), workingHours + 1);
+        });
+
+        // compare each teacher to his id by the map. see for match.
+        penalty = solution.getGens().getProblem().getTeachers().stream()
+                .mapToInt(teacher -> Math.abs(teacher.getWorkingHours() - teacherID2workingHours.get(teacher.getId())))
+                .sum();
+
+        if (penalty > max || max == 0) {
+            return 0;
+        }
+
+        return ((float) (max - penalty) / max) * MAX_PERCENTAGE;
     }
 }
