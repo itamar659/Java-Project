fdaee07c848e54142cfd8810fb9dc2d0c4fb7b4a
Daniel Caspit <dcaspit@gmail.com>
2021-10-02 14:28:02 +0300

    koko

--- Exercise 3/WebSite/web/evolution/evolution.js
+++ Exercise 3/WebSite/web/evolution/evolution.js
@@ -334,7 +334,62 @@ function objectifyForm(formArray) {
     for (var i = 0; i < formArray.length; i++){
         returnArray[formArray[i]['name']] = formArray[i]['value'];
     }
-    return returnArray;
+    return arrangeJson(returnArray);
+}
+
+
+function arrangeJson(data){
+    var initMatch = /^([a-z0-9]+?)\[/i;
+    var first = /^\[[a-z0-9]+?\]/i;
+    var isNumber = /^[0-9]$/;
+    var bracers = /[\[\]]/g;
+    var splitter = /\]\[|\[|\]/g;
+
+    for(var key in data) {
+        if(initMatch.test(key)){
+            data[key.replace(initMatch,'[$1][')] = data[key];
+        }
+        else{
+            data[key.replace(/^(.+)$/,'[$1]')] = data[key];
+        }
+        delete data[key];
+    }
+
+
+    for (var key in data) {
+        processExpression(data, key, data[key]);
+        delete data[key];
+    }
+
+    function processExpression(dataNode, key, value){
+        var e = key.split(splitter);
+        if(e){
+            var e2 =[];
+            for (var i = 0; i < e.length; i++) {
+                if(e[i]!==''){e2.push(e[i]);}
+            }
+            e = e2;
+            if(e.length > 1){
+                var x = e[0];
+                var target = dataNode[x];
+                if(!target){
+                    if(isNumber.test(e[1])){
+                        dataNode[x] = [];
+                    }
+                    else{
+                        dataNode[x] ={}
+                    }
+                }
+                processExpression(dataNode[x], key.replace(first,''), value);
+            }
+            else if(e.length == 1){
+                dataNode[e[0]] = value;
+            }
+            else{
+                alert('This should not happen...');
+            }
+        }
+    }
 }
 
 function setCheckBoxChanges() {
@@ -546,6 +601,8 @@ function addMutation(){
     // }
     // else{
         var d = $("#mutation-div-body").clone().attr('id', 'mutation-div-body' + mutationsAdded).removeClass("hider").appendTo("#mutation-div");
+        $(d.find('#selectMutation')).attr('name', 'mutation[' + mutationsAdded + '][name]');
+        $(d.find('#mutation-maxTupples')).attr('name', 'mutation[' + mutationsAdded + '][maxTupples]');
  //   }
 
     mutationsAdded++;
